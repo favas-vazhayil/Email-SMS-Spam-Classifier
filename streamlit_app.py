24155fd3cf52c9ef9ce19ae16b30e0dbcd37a99f
favas-vazhayil <141208579+favas-vazhayil@users.noreply.github.com>
2023-08-04 14:55:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,8 @@ page_bg_img = '''
 background-image: url("https://plus.unsplash.com/premium_photo-1682309572625-791e25352998?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1512&q=80");
 background-size: cover;
 }
+[data-testid="stHeader"]{
+background-color: rgba(0,0,0,0);
 </style>
 '''
 
