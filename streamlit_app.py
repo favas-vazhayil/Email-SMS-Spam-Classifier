10623f5441e415c7897a665c64f076983dc0771f
favas-vazhayil <141208579+favas-vazhayil@users.noreply.github.com>
2023-08-04 14:55:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def transform_text(text):
 
 tfidf=pickle.load(open('vectorizer.pkl','rb'))
 model=pickle.load(open('model.pkl','rb'))
-st.title('red['Email/SMS Spam Classifier']')
+st.title('Email/SMS Spam Classifier')
 
 input_sms = st.text_area("Enter the message ")
 
